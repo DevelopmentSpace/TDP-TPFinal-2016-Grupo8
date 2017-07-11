c66aee916ce22a44827f68ecb392faead60187b2
Manuel Chichi <manuchichi@hotmail.com>
2017-07-11 18:15:37 -0300

    PlanteoDelCampaignService

---
+++ TPFinal/TPFinal/Model/CampaignService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TPFinal.Domain;
+using TPFinal.DAL;
+
+namespace TPFinal.Model
+{
+    class CampaignService
+    {
+        private IList<Campaign> iCampaignList;
+        private int iActualImage = 0;
+        private int iActualCampaign = 0;
+
+        public void Create(CampaignDTO pCampaignDTO)
+        {
+
+
+        }
+
+        public void Update(CampaignDTO pCampaignDTO)
+        {
+
+
+        }
+
+        public void Delete(CampaignDTO pCampaignDTO)
+        {
+
+
+        }
+
+        public void GetActualImage()
+        {
+
+
+        }
+
+        public void Start()
+        {
+            DateTime pDateFrom = DateTime.Now;
+            DateTime pDateTo = DateTime.Now.AddHours(1);
+
+            iCampaignList = IUnitOfWork.CampaignRepository.GetActives(pDateFrom,pDateTo);
+
+
+
+        }
+
+        public void Stop()
+        {
+
+
+        }
+
+        private void OnIntervalTimer()
+        {
+
+        }
+
+        private void OnRefreshTimer()
+        {
+
+        }
+
+
+    }
+}
