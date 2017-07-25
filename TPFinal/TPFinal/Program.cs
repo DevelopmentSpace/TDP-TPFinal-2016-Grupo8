237fbb6509c49660b60b6206e990a01f8fc2c3f8
Manuel Chichi <manuchichi@hotmail.com>
2017-07-25 20:49:51 -0300

    Cosas del logger. Probando si funciona

--- TPFinal/TPFinal/Program.cs
+++ TPFinal/TPFinal/Program.cs
@@ -7,12 +7,14 @@ namespace TPFinal
 {
     static class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            
             Common.Logging.LogManager.Adapter = new Common.Logging.Simple.ConsoleOutLoggerFactoryAdapter { Level = Common.Logging.LogLevel.Info };
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
