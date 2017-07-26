1e7b2b1017221902336540da17b1c6f794db2ac2
Black <Black@BLACK-NET>
2017-07-26 06:24:24 -0300

    Comentada

--- TPFinal/TPFinal/Model/ICampaignService.cs
+++ TPFinal/TPFinal/Model/ICampaignService.cs
@@ -9,18 +9,46 @@ namespace TPFinal.Model
 {
     interface ICampaignService : IObservable
     {
+		/// <summary>
+		/// Agrega una campaña
+		/// </summary>
+		/// <param name="pCampaignDTO">Campaña a agregar</param>
         void Create(CampaignDTO pCampaignDTO);
 
+		/// <summary>
+		/// Elimina una campaña
+		/// </summary>
+		/// <param name="pCampaignDTO">Campaña a eliminar</param>
         void Update(CampaignDTO pCampaignDTO);
 
+		/// <summary>
+		/// Elimina una campaña
+		/// </summary>
+		/// <param name="pId">Id de la campaña a eliminar</param>
         void Delete(int pId);
 
+		/// <summary>
+		/// Obtiene una campaña
+		/// </summary>
+		/// <param name="pId">Id de la campaña</param>
+		/// <returns>Campaña con Id dado</returns>
         CampaignDTO GetCampaign(int pId);
 
+		/// <summary>
+		/// Obtiene todas las campañas
+		/// </summary>
+		/// <returns>Enumerable con campañas</returns>
         IEnumerable<CampaignDTO> GetAll();
 
+		/// <summary>
+		/// Obtiene la imagen actual a mostrar
+		/// </summary>
+		/// <returns>Imagen a mostrar</returns>
         ByteImageDTO GetActualImage();
 
+		/// <summary>
+		/// Actualiza las campañas con la base de datos.
+		/// </summary>
         void ForceUpdate();
     }
 }
