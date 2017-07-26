aa8b774193648ef25d6f35e884c626b34912e901
Manuel Chichi <manuchichi@hotmail.com>
2017-07-26 06:22:20 -0300

    BannerService comentado

--- TPFinal/TPFinal/Model/IBannerService.cs
+++ TPFinal/TPFinal/Model/IBannerService.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace TPFinal.Model
 {
-    interface IBannerService : IObservable
+    /// <summary>
+    /// Interfaz del servicio de banners
+    /// </summary>
+    interface IBannerService
     {
+        /// <summary>
+        /// Funcion que agrega servicios que brinden textos a el servicio de banner.
+        /// </summary>
+        /// <param name="textBanner">Servicio de textos</param>
         void AddService(ITextBanner textBanner);
 
+        /// <summary>
+        /// Funcion que otorga todos los textos activos en ese momento de todos los servicios de textos.
+        /// </summary>
+        /// <returns>Texto que tienen todos los banners activos</returns>
         String GetText();
 
+        /// <summary>
+        /// Funcion que realiza una actualizacion forzosa con la base de datos y actualiza los banners de los servicios.
+        /// </summary>
         void ForceUpdate();
     }
 }
